8af6c22c4c4509571f8086edfed6f8aa69354c8a
Martin Georgiev <martin.georgiev@softwaregroup-bg.com>
2014-12-15 15:17:33 +0200

    ut-port-sql

--- index.js
+++ index.js
@@ -1,83 +1,88 @@
-/**
- * @module sql
- * @author UT Route Team
- * @description SQL module
- * @requires mssql
- * @requires when
- */
-var sql = require('mssql');
-var when = require('when');
-
-/**
- * @class SQL
- * @description Constructor method initiating the database connection
- * @param {Object} config Configuration paramters to connect to SQL database
- * $returns {Object} The current instance
- */
-function SQL(config, validator, logger) {
+(function(define){ define(function(require) {
     /**
-     * @function val
-     * @description Empty validation method
+     * @module sql
+     * @author UT Route Team
+     * @description SQL module
+     * @requires mssql
+     * @requires when
      */
-    this.val = validator;
+    var sql = require('mssql');
+    var when = require('when');
+
     /**
-     * @function log
-     * @description Empty logger method
+     * @class SQL
+     * @tutorial sql
+     * @description Constructor method initiating the database connection
+     * @param {Object} config Configuration paramters to connect to SQL database
+     * $returns {Object} The current instance
      */
-    this.log = logger;
-    /**
-     * @param {Object} connection
-     * @description SQL connection
-     */
-    this.connection = new sql.Connection(config, function(err) {
-        if (err) throw err;
-    });
 
-    return this;
-}
-/**
- * @function exec
- * @description Handles SQL query execution
- * @param {Object} data
- * @returns {Promise} Returns a promise to be handled after being executed
- */
-SQL.prototype.exec = function(data) {
-
-    if (typeof this.val === 'function') {
-        this.val(data);
-    }
-
-    var request = new sql.Request(this.connection);
-
-    return when.promise(function (resolve, reject) {
-        request.query(data._sql.sql, function (err, recordset) {
-            if (err) {
-                reject({
-                    _error: {
-                        message: err.message,
-                        query: data._sql.sql
-                    }
-                });
-            } else {
-                switch (data._sql.process) {
-                    case 'return':
-                        var response = {}
-                        Object.keys(data).forEach(function(value) {
-                            if (value !== '_sql') {
-                                response[value] = data[value];
-                            }
-                        });
-                        if (recordset.length) {
-                            Object.keys(recordset[0]).forEach(function(value) {
-                                response[value] = recordset[0][value];
-                            });
-                        }
-                        resolve(response);
-                        break;
-                }
-            }
+    function SQL(config, validator, logger) {
+        /**
+         * @function val
+         * @description Empty validation method
+         */
+        this.val = validator;
+        /**
+         * @function log
+         * @description Empty logger method
+         */
+        this.log = logger;
+        /**
+         * @param {Object} connection
+         * @description SQL connection
+         */
+        this.connection = new sql.Connection(config, function(err) {
+            if (err) throw err;
         });
-    });
-};
 
-module.exports = SQL;
+        return this;
+    }
+    /**
+     * @function exec
+     * @description Handles SQL query execution
+     * @param {Object} data
+     * @returns {Promise} Returns a promise to be handled after being executed
+     */
+    SQL.prototype.exec = function(data) {
+
+        if (typeof this.val === 'function') {
+            this.val(data);
+        }
+
+        var request = new sql.Request(this.connection);
+
+        return when.promise(function (resolve, reject) {
+            request.query(data._sql.sql, function (err, recordset) {
+                if (err) {
+                    reject({
+                        _error: {
+                            message: err.message,
+                            query: data._sql.sql
+                        }
+                    });
+                } else {
+                    switch (data._sql.process) {
+                        case 'return':
+                            var response = {}
+                            Object.keys(data).forEach(function(value) {
+                                if (value !== '_sql') {
+                                    response[value] = data[value];
+                                }
+                            });
+                            if (recordset.length) {
+                                Object.keys(recordset[0]).forEach(function(value) {
+                                    response[value] = recordset[0][value];
+                                });
+                            }
+                            resolve(response);
+                            break;
+                    }
+                }
+            });
+        });
+    };
+
+    return SQL;
+
+});})(typeof define === 'function' && define.amd ?  define : function(factory){ module.exports = factory(require); });
--- test/test.js
+++ test/test.js
@@ -1,50 +1,71 @@
-var SQL = require('../');
 //var mocha = require('mocha');
 //var expect = require('chai').expect;
 //var assert = require('chai').assert;
+var wire = require('wire');
 
+m = wire({
+    winston : {
+        create: {
+            module: 'ut-log',
+            args: {
+                type: 'winston',
+                name: 'winston_test',
+                dependencies: [],
+                transports: {
+                    'file': {
+                        filename: './winstonTest.log',
+                        level: 'trace'
+                    },
+                    'console': {
+                        colorize: 'true',
+                        level: 'trace'
+                    }
+                }
+            }
+        }
+    },
+    sql: {
+        create: {
+            module: 'ut-port-sql',
+            args: [
+                {
+                    user: 'switch',
+                    password: 'switch',
+                    server: '192.168.133.40',
+                    database: 'G_MFSP3_2'
+                },
+                require('ut-validate').get('joi').validateSql,
+                {$ref: 'winston'}
+            ]
+        }
+    }
+}, {require : require});
 
-return false;
-describe('Query Positive', function() {
-    it('Running a successful query', function() {
-        // test validation
-        expect(sql.val({
-            _sql: {
-                process: 'return',
-                sql: 'select * from Account'
-            },
-            what: 'v1',
-            ever: 'v2'
-        })).to.be.equal(true);
-        // test result
-        sql.exec({
-            _sql: {
-                process: 'return',
-                sql: 'select * from Account'
-            },
-            what: 'v1',
-            ever: 'v2'
-        }).then(function(msg) {
-            msg.deepEqual({ what: 'v1', ever: 'v2', number: 1, number2: 2 });
-        }).catch(function(err) {
-            err.deepEqual({ what: 'v1', ever: 'v2', number: 1, number2: 2 });
-        });
-    })
-});
+m.then(function(c) {
+    var sql = c.sql;
+    console.log(sql.val({
+        _sql: {
+            process: 'return',
+            sql: 'select * from Account'
+        },
+        what: 'v1',
+        ever: 'v2'
+    }));
 
-describe('Query Negative', function() {
-    it('Running unsuccessful query', function() {
-        sql.exec({
-            _sql: {
-                process: 'return',
-                sql: 'select * from Account'
-            },
-            what: 'v1',
-            ever: 'v2'
-        }).then(function(msg) {
-            msg.notDeepEqual({ what: 'v1s', ever: 'v2', number: 1, number2: 2 });
-        }).catch(function(err) {
-            console.log(err);
-        });
+    sql.exec({
+        _sql: {
+            process: 'return',
+            sql: 'select * from Account'
+        },
+        what: 'v1',
+        ever: 'v2'
+    }).then(function(msg) {
+        console.log(msg);
+    }).catch(function(err) {
+        console.log(msg);
     });
+
+}).otherwise(function(error){
+    err = error;
+    console.log(err)
 });
