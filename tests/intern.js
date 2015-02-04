032691b091654544f2fe193f0a8f9807417484c3
Martin Georgiev <martin.georgiev@softwaregroup-bg.com>
2015-02-04 14:04:17 +0200

    package.json

---
+++ tests/intern.js
@@ -0,0 +1,9 @@
+/**
+ * Run tests with intern-client config=test/intern.js to run all tests
+ * If individual testing is required copy/paste intern.js with different name and change the command
+ */
+define({
+	suites: ['tests/test.sql.js'],
+	excludeInstrumentation: /^(?:test)\//,
+    reporters: ['console', 'cobertura', 'junit', 'lcovhtml']
+});
