93aa0f874d0fc6ebed91c4c50ac55b9e0d6e1296
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-06-16 08:33:04 +0300

    feat:set database options

--- sql.js
+++ sql.js
@@ -201,6 +201,8 @@ module.exports = {
         IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = '${name}')
         BEGIN
           CREATE DATABASE [${name}]
+          ALTER DATABASE [${name}] SET READ_COMMITTED_SNAPSHOT ON
+          ALTER DATABASE [${name}] SET AUTO_SHRINK OFF
         END`;
     },
     createUser: function(name, user, password) {
