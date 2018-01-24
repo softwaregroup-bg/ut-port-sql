c2b6be9715355fb22496e652161adca86a4cf6e8
miroslav.lessev <miroslav.lessev@softwaregroup-bg.com>
2018-01-24 16:45:49 +0200

    fix: UTP-1256 string operator to single quote

--- index.js
+++ index.js
@@ -1134,7 +1134,7 @@ module.exports = function({parent}) {
         this.checkConnection();
         let request = this.getRequest();
         request.multiple = true;
-        return request.query(mssqlQueries.loadSchema(this.config.updates === false || this.config.updates === "false")).then(function(result) {
+        return request.query(mssqlQueries.loadSchema(this.config.updates === false || this.config.updates === 'false')).then(function(result) {
             let schema = {source: {}, parseList: [], types: {}, deps: {}};
             result[0].reduce(function(prev, cur) { // extract source code of procedures, views, functions, triggers
                 let full = cur.full;
