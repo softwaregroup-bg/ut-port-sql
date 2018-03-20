181a0216a1a2be382ba4cf77942d42b336ccb230
ranjith.pandiyan <ranjith.pandiyan@softwaregroup-bg.com>
2018-03-20 16:55:50 +0530

    refactor: replace newline char with EOL

--- saveAs/csv.js
+++ saveAs/csv.js
@@ -1,21 +1,21 @@
+const EOL = require('os').EOL;
 var Transform = require('./transform');
 let { getResultSetName } = require('./helpers');
 class CsvTransform extends Transform {
     constructor(stream, config) {
         super(stream, config);
         this.options = {
-            canPushRow: !config.namedSet,
-            newLine: '\n'
+            canPushRow: !config.namedSet
         };
     }
     onStart() {
         let { columns } = this.config;
-        this.stream.push((columns || []).map((col) => `"${col.displayName}"`).join(this.config.seperator || ',') + this.options.newLine);
+        this.stream.push((columns || []).map((col) => `"${col.displayName}"`).join(this.config.seperator || ',') + EOL);
     }
     onRow(chunk) {
         this.options.canPushRow && this.stream.push((this.config.columns || []).map((col) => {
             return ['"', col.transform ? col.transform(chunk[col.name], chunk) : chunk[col.name], '"'].join('');
-        }).join(this.config.seperator || ',') + this.options.newLine);
+        }).join(this.config.seperator || ',') + EOL);
     }
     onResultSet(chunk) {
         let { resultSetName, namedSet } = this.config;
