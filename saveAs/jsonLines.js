68fa261aa3f6f5ab13378d55d2e0cbed08d826c1
kristiyan.todorov <kristiyan.todorov@softwaregroup-bg.com>
2018-11-12 11:15:59 +0200

    feat: json lines format

---
+++ saveAs/jsonLines.js
@@ -0,0 +1,19 @@
+var Transform = require('./transform');
+let { getResultSetName } = require('./helpers');
+
+module.exports = class JsonLinesTransform extends Transform {
+    constructor(stream, config) {
+        if (!config.lineSeparator || typeof config.lineSeparator !== 'string') {
+            config.lineSeparator = '\r\n';
+        }
+        super(stream, config);
+    }
+    onRow(chunk) {
+        let jsonString = JSON.stringify({[this.currentResultset]: chunk});
+        let line = `${jsonString}${this.config.lineSeparator}`;
+        this.stream.push(line);
+    }
+    onResultSet(chunk) {
+        this.currentResultset = getResultSetName(chunk);
+    }
+}
