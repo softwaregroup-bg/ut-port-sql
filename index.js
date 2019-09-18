3fec6be8ef3b3774bf5d794082572d4178e2c1a6
stambata <stamen.peev@softwaregroup-bg.com>
2019-09-18 14:32:10 +0300

    fix: always delete ***.json columns from resultSet (#83)

--- index.js
+++ index.js
@@ -1037,9 +1037,8 @@ module.exports = function({utPort}) {
                                         }
                                     });
                                     jsonColumns.forEach(function(key) {
-                                        if (record[key]) { // value is not null
-                                            record[key.substr(0, key.length - 5)] = JSON.parse(record[key]);
-                                        }
+                                        record[key.substr(0, key.length - 5)] = record[key] ? JSON.parse(record[key]) : record[key];
+                                        delete record[key];
                                     });
                                     xmlColumns.forEach(function(key) {
                                         if (record[key]) { // value is not null
