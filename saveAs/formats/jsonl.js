86b1b3f11801fa7d523a1b43720e5703367ae648
stamen.peev <stamen.peev@softwaregroup-bg.com>
2019-10-09 16:45:59 +0300

    chore: add forgotten files

---
+++ saveAs/formats/jsonl.js
@@ -0,0 +1,16 @@
+const Format = require('./format');
+
+module.exports = class JsonLFormat extends Format {
+    constructor(config) {
+        super(config);
+        this.resultSet = {};
+        if (typeof this.config.lineSeparator !== 'string') this.config.lineSeparator = '\r\n';
+    }
+    onResultSet(resultSet) {
+        this.resultSet = resultSet;
+    }
+    onRow(row) {
+        const line = JSON.stringify({[this.resultSet.resultSetName]: row});
+        this.write(`${line}${this.config.lineSeparator}`);
+    }
+};
