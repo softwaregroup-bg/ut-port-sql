3a9a96b8665b8d44968406e00b889c0ed5ca3027
stamen.peev <stamen.peev@softwaregroup-bg.com>
2019-10-09 16:30:24 +0300

    fix: handle errors

--- saveAs/index.js
+++ saveAs/index.js
@@ -1,13 +1,102 @@
+const path = require('path');
+const fsplus = require('fs-plus');
+const fs = require('fs');
 const formats = {
-    jsonl: require('./jsonl'),
-    json: require('./json'),
-    csv: require('./csv')
+    jsonl: require('./formats/jsonl'),
+    json: require('./formats/json'),
+    csv: require('./formats/csv')
 };
 
-module.exports = (port, request, filename = {}) => {
-    const config = typeof filename === 'string' ? {filename} : filename;
+module.exports = async(port, request, { saveAs }, name) => {
+    const config = typeof saveAs === 'string' ? { filename: saveAs } : saveAs;
+    if (path.isAbsolute(config.filename)) throw this.errors['portSQL.absolutePath']();
+
+    const baseDir = path.join(port.bus.config.workDir, 'ut-port-sql', 'export');
+    const outputFilePath = path.resolve(baseDir, config.filename);
+    if (!outputFilePath.startsWith(baseDir)) throw port.errors['portSQL.invalidFileLocation']();
+
+    await new Promise((resolve, reject) => {
+        fsplus.makeTree(path.dirname(outputFilePath), err => {
+            if (!err || err.code === 'EEXIST') {
+                return resolve();
+            }
+            return reject(err);
+        });
+    });
+
+    request.stream = true;
     const ext = config.filename.split('.').pop();
     const Format = formats[ext] || formats.jsonl;
-    const format = new Format(port, request, config);
-    return format;
+    const formatter = new Format(config);
+    const writer = fs.createWriteStream(outputFilePath);
+
+    return new Promise((resolve, reject) => {
+        let replied = false;
+
+        const reply = err => {
+            if (replied) return;
+            replied = true;
+            writer.end();
+            if (!err) {
+                try {
+                    formatter.onDone();
+                    return resolve({outputFilePath});
+                } catch (e) {
+                    err = e;
+                }
+            } else {
+                request.cancel();
+            }
+            port.log.error && port.log.error(err);
+            try {
+                fs.unlinkSync(outputFilePath);
+            } catch (e) {
+                port.log.error && port.log.error(e);
+            }
+            reject(port.errors['portSQL.exportError'](err));
+        };
+
+        const wrap = fn => async(...params) => {
+            try {
+                await fn(...params);
+            } catch (e) {
+                reply(e);
+            }
+        };
+
+        let transform;
+
+        formatter.on('data', data => {
+            writer.write(data);
+        });
+
+        request.on('recordset', wrap(columns => {
+            transform = port.getRowTransformer(columns);
+        }));
+
+        request.on('row', wrap(async row => {
+            if (row.resultSetName) {
+                formatter.onResultSet(row);
+            } else {
+                if (typeof transform === 'function') {
+                    if (transform.constructor.name !== 'AsyncFunction') {
+                        request.pause();
+                        await transform(row);
+                        request.resume();
+                    } else {
+                        transform(row);
+                    }
+                }
+                formatter.onRow(row);
+            }
+        }));
+
+        request.on('error', reply);
+
+        request.on('done', wrap(() => {
+            reply();
+        }));
+
+        request.execute(name);
+    });
 };
