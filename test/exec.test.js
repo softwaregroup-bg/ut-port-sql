bb141a785ec7b9456d7b6366bc1a4c70b8c626bc
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2020-03-02 16:24:27 +1100

    chore: build

--- test/exec.test.js
+++ test/exec.test.js
@@ -6,7 +6,15 @@ require('ut-run').run({
         implementation: 'port-sql',
         db: {
             allowQuery: true,
-            logLevel: 'error'
+            logLevel: 'error',
+            connection: {
+                server: 'infradb14',
+                user: '${decrypt("3b280fb6a2c0c22483dfb73be18128774fa156653edd29eebed4f3c4e8f5c0fa")}',
+                password: '${decrypt("de763840f0dc08b85b0d845b17d15e1bdaf6a774dc4eecf32e368620b7b7d410")}'
+            },
+            create: {
+                user: '${decrypt("289fd8ff4717c56d59b1ebc6987fbd1f1f0df4849705f6216b319763c8edb252")}'
+            }
         }
     },
     params: {
