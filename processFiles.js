9772472f09013b66b965145c3c6f46c5bee65847
Kristiyan Todorov <kristiyan.todorov@softwaregroup-bg.com>
2021-10-11 23:09:57 +0300

    fix: pass default value (#108)
    * fix: pass default value
    
    * chore: cleanup

--- processFiles.js
+++ processFiles.js
@@ -159,9 +159,8 @@ function shouldCreateTT(schemaConfig, tableName) {
 
 function interpolate(txt, params = {}) {
     return txt.replace(VAR_RE, (placeHolder, label) => {
-        const value = dotProp.get(params, label);
+        const value = dotProp.get(params, label, placeHolder);
         switch (typeof value) {
-            case 'undefined': return placeHolder;
             case 'object': return JSON.stringify(value);
             default: return value;
         }
