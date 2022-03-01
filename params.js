bf648b8b7fe58684475969a926bb13b06fe0fd7f
Kalin Krustev <kalin.krustev@gmail.com>
2022-03-01 07:48:59 +0100

    Minor/tags (#113)
    * feat: allow mixed parameter naming
    
    * feat: improve logging of decrypt errors
    
    * feat: tag indexing
    
    * chore: remove flatten
    
    * [ci-skip][ci skip] version incremented to 7.43.0-tags.0
    
    * fix: do not remove auth
    
    * [ci-skip][ci skip] version incremented to 7.43.0-tags.1
    
    Co-authored-by: Jenkins CI <jenkins.ci@softwaregroup-bg.com>

--- params.js
+++ params.js
@@ -1,18 +1,25 @@
 const ROW_VERSION_INNER_TYPE = 'BINARY';
 const xml2js = require('xml2js');
 const mssql = require('mssql');
+const lodashGet = require('lodash.get');
 const xmlBuilder = new xml2js.Builder({headless: true});
 const isEncrypted = item => item && ((item.def && item.def.type === 'varbinary' && item.def.size % 16 === 0) || (item.length % 16 === 0) || /^encrypted/.test(item.name));
 const WORDS = /(\p{Letter}|\d)+/gu;
 const LETTER = /\p{Letter}|\d/gu;
+const TAGS = /(\p{Letter}|\d|=|\.)+/gu;
 
 function addNgram(hmac, ngramParam, add, row, param, column, string) {
     const options = ngramParam.options && ngramParam.options[column];
     if (!options) return ngramParam;
     const id = typeof options === 'number' ? options : options.id;
     const unique = new Set();
-    const match = string.toLowerCase().match(WORDS);
+    const tags = options.tags || column?.endsWith?.('Tags');
+    const match = string.toLowerCase().match(tags ? TAGS : WORDS);
     match && match.forEach(word => {
+        if (tags) {
+            unique.add(word);
+            return;
+        }
         word = word.match(LETTER);
         if (word) {
             const {min = 3, max = 3, depth = word.length - min} = options;
@@ -32,15 +39,17 @@ function addNgram(hmac, ngramParam, add, row, param, column, string) {
 }
 
 function getValue(cbc, hmac, ngram, index, param, column, value, def, updated) {
+    const calcNgram = what => what && ngram && addNgram(hmac, ngram, ngram.add, index, param.name, param.name + '.' + column.name, what);
     if (updated) {
         return updated;
     }
     if (value === undefined) {
+        calcNgram(def);
         return def;
     } else if (value) {
         if (cbc && isEncrypted({name: column.name, def: {type: column.type.declaration, size: column.length}})) {
             if (!Buffer.isBuffer(value) && !(value instanceof Date) && typeof value === 'object') value = JSON.stringify(value);
-            ngram && addNgram(hmac, ngram, ngram.add, index, param.name, param.name + '.' + column.name, value);
+            calcNgram(value);
             return cbc.encrypt(value, column.name);
         } else if (/^(date.*|smalldate.*)$/.test(column.type.declaration)) {
             // set a javascript date for 'date', 'datetime', 'datetime2' 'smalldatetime'
@@ -59,41 +68,8 @@ function getValue(cbc, hmac, ngram, index, param, column, value, def, updated) {
             return JSON.stringify(value);
         }
     }
+    calcNgram(value);
     return value;
-}
-
-function flattenMessage(data, delimiter, limit) {
-    if (!delimiter) {
-        return data;
-    }
-    const result = {};
-    function flatten(cur, prop, depth) {
-        if (depth > limit) throw new Error('Unsupported deep nesting for property ' + prop);
-        if (typeof cur === 'function') {
-            ;
-        } else if (Object(cur) !== cur) {
-            result[prop] = cur;
-        } else if (Array.isArray(cur) || Buffer.isBuffer(cur)) {
-            // for (let i = 0, l = cur.length; i < l; i += 1) {
-            //     flat(cur[i], prop + '[' + i + ']');
-            // }
-            // if (l === 0) {
-            //     result[prop] = [];
-            // }
-            result[prop] = cur;
-        } else {
-            let isEmpty = true;
-            for (const p in cur) {
-                isEmpty = false;
-                flatten(cur[p], prop ? prop + delimiter + p : p, depth + 1);
-            }
-            if (isEmpty && prop) {
-                result[prop] = {};
-            }
-        }
-    }
-    flatten(data, '', 1);
-    return result;
 }
 
 function sqlType(def) {
@@ -145,7 +121,6 @@ function setParam(cbc, hmac, ngram, request, param, value, limit) {
                             addNgram(hmac, param, (...columns) => type.rows.add(...columns), 1, 'search', row[0], row[1]);
                             return;
                         }
-                        row = flattenMessage(row, param.flatten, limit);
                         if (typeof row === 'object') {
                             type.rows.add(...param.columns.map((column, index) => getValue(
                                 cbc,
@@ -154,7 +129,7 @@ function setParam(cbc, hmac, ngram, request, param, value, limit) {
                                 rowIndex + 1,
                                 param,
                                 type.columns[index],
-                                row[column.column],
+                                param.flatten ? lodashGet(row, column.column.split(param.flatten)) : row[column.column],
                                 column.default,
                                 column.update && Object.prototype.hasOwnProperty.call(row, column.update)
                             )));
@@ -173,7 +148,6 @@ function setParam(cbc, hmac, ngram, request, param, value, limit) {
                         }
                     });
                 } else if (typeof value === 'object') {
-                    value = flattenMessage(value, param.flatten, limit);
                     type.rows.add(...param.columns.map((column, index) => getValue(
                         cbc,
                         hmac,
@@ -181,12 +155,11 @@ function setParam(cbc, hmac, ngram, request, param, value, limit) {
                         1,
                         param,
                         type.columns[index],
-                        value[column.column],
+                        param.flatten ? lodashGet(value, column.column.split(param.flatten)) : value[column.column],
                         column.default,
                         column.update && Object.prototype.hasOwnProperty.call(value, column.update)
                     )));
                 } else {
-                    value = flattenMessage(value, param.flatten, limit);
                     type.rows.add(getValue(
                         cbc,
                         hmac,
@@ -210,4 +183,4 @@ function setParam(cbc, hmac, ngram, request, param, value, limit) {
     return value;
 }
 
-module.exports = {setParam, isEncrypted, flattenMessage};
+module.exports = {setParam, isEncrypted};
