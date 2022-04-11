4d23be50cce64fb023c7c1b39b6ed5437a96907a
Tanya Rasheva <tanya.rasheva@softwaregroup.com>
2022-04-11 16:17:01 +0300

    fix: re-throw the error, AAA-000

--- processFiles.js
+++ processFiles.js
@@ -35,7 +35,8 @@ function replaceCoreError(statement, fileName, objectName, params) {
         .map((line, index) => (line.replace(CORE_ERROR, (match, ret, type) =>
             `DECLARE @CORE_ERROR_FILE_${index} sysname='${fileName.replace(/'/g, '\'\'')}' ` +
             `DECLARE @CORE_ERROR_LINE_${index} int='${index + 1}' ` +
-            `${ret || ''} EXEC [core].[errorStack] @procid=@@PROCID, @file=@CORE_ERROR_FILE_${index}, @fileLine=@CORE_ERROR_LINE_${index}, @params = ${params}${type ? `, ${type}` : ''}`)))
+            `${ret || ''} EXEC [core].[errorStack] @procid=@@PROCID, @file=@CORE_ERROR_FILE_${index},
+             @fileLine=@CORE_ERROR_LINE_${index}, @params = ${params}${type ? `, ${type}` : '; THROW'}`)))
         .join('\n');
 }
 
