cc5954771943b02a7a2c1e20c2e6250e1f7d8549
Tanya Rasheva <tanya.rasheva@softwaregroup.com>
2022-04-15 18:15:29 +0300

    chore: remove space

--- processFiles.js
+++ processFiles.js
@@ -34,7 +34,7 @@ function replaceCoreError(statement, fileName, objectName, params) {
         .split('\n')
         .map((line, index) => (line.replace(CORE_ERROR, (match, ret, type) =>
             `DECLARE @CORE_ERROR_FILE_${index} sysname='${fileName.replace(/'/g, '\'\'')}' ` +
-            `DECLARE @CORE_ERROR_LINE_${index} int='${index + 1}' ` + 
+            `DECLARE @CORE_ERROR_LINE_${index} int='${index + 1}' ` +
             `${ret || ''} EXEC [core].[errorStack] @procid=@@PROCID, @file=@CORE_ERROR_FILE_${index}, @fileLine=@CORE_ERROR_LINE_${index}, @params = ${params}${type ? `, ${type}` : ', @useRethrow = 1;\nTHROW'}`)))
         .join('\n');
 }
