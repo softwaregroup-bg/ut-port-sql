2ac56829c42ed3990c96f30a7134e302b575bd24
Kalin Krustev <kalin.krustev@gmail.com>
2022-04-17 11:04:52 +0300

    chore: #UTCORE-121

--- test/oracle.test.js
+++ test/oracle.test.js
@@ -39,11 +39,11 @@ require('ut-run').run({
                 driver: 'oracle',
                 domain: 'softwaregroup-bg.com',
                 server: 'bgs-vlx-db-05.softwaregroup-bg.com:1521',
-                user: '${decrypt("3b280fb6a2c0c22483dfb73be18128774fa156653edd29eebed4f3c4e8f5c0fa")}',
-                password: '${decrypt("de763840f0dc08b85b0d845b17d15e1bdaf6a774dc4eecf32e368620b7b7d410")}'
+                user: '${decrypt(\'3b280fb6a2c0c22483dfb73be18128774fa156653edd29eebed4f3c4e8f5c0fa\')}',
+                password: '${decrypt(\'de763840f0dc08b85b0d845b17d15e1bdaf6a774dc4eecf32e368620b7b7d410\')}'
             },
             create: {
-                user: '${decrypt("289fd8ff4717c56d59b1ebc6987fbd1f1f0df4849705f6216b319763c8edb252")}'
+                user: '${decrypt(\'289fd8ff4717c56d59b1ebc6987fbd1f1f0df4849705f6216b319763c8edb252\')}'
             }
         }
     },
