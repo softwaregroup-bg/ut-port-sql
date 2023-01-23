0a08c98678bbdd0bd091c73f8755a1d2f62de4a4
Kalin Krustev <kalin.krustev@gmail.com>
2023-01-23 10:52:04 +0200

    test: call stack

--- test/exec.test.js
+++ test/exec.test.js
@@ -50,20 +50,18 @@ require('ut-run').run({
             cbc: '75742d706f72742d73716c2121212d2d2d2d75742d706f72742d73716c212121',
             hmac: '75742d706f72742d73716c2121212d2d2d2d75742d706f72742d73716c212121',
             connection: {
-                server: 'bgs-vws-db-10.softwaregroup-bg.com',
-                user: 'kalin.krustev',
-                password: 'kalin@bgs-vws-db-10'
+                server: 'infradb14',
+                user: '${decrypt(\'3b280fb6a2c0c22483dfb73be18128774fa156653edd29eebed4f3c4e8f5c0fa\')}',
+                password: '${decrypt(\'de763840f0dc08b85b0d845b17d15e1bdaf6a774dc4eecf32e368620b7b7d410\')}'
             },
             create: {
-                user: 'ut5',
-                password: 'ut5'
+                user: '${decrypt(\'289fd8ff4717c56d59b1ebc6987fbd1f1f0df4849705f6216b319763c8edb252\')}'
             }
         },
         utRun: {
             test: {
                 jobs: 'test'
-            },
-            logLevel: 'trace'
+            }
         }
     }
 });
